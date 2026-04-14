a33381cb12f028199d701c9d7a4e844480ce32d1
JaiderSanchez <jaiderfabiansd@gmail.com>
2026-04-13 22:17:36 -0500

    First commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// const sections = document.querySelectorAll('.fade-section');
+
+// const observer = new IntersectionObserver((entries) => {
+//     entries.forEach(entry => {
+//         if (entry.isIntersecting) {
+//             entry.target.classList.add('visible');
+//         } else {
+//             entry.target.classList.remove('visible'); // para animar también al subir
+//         }
+//     });
+// }, {
+//     threshold: 0.2
+// });
+
+// sections.forEach(section => {
+//     observer.observe(section);
+// });
