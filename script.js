fbcc7895e8cf206f5df959344b6beb7e9bdc96ba
JaiderSanchez <jaiderfabiansd@gmail.com>
2026-04-13 23:41:15 -0500

    I made progress in developing the HTML section: Verses that Transform. I also made some modifications to the JS code.

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-// const sections = document.querySelectorAll('.fade-section');
+const sections = document.querySelectorAll('.fade-section');
 
-// const observer = new IntersectionObserver((entries) => {
-//     entries.forEach(entry => {
-//         if (entry.isIntersecting) {
-//             entry.target.classList.add('visible');
-//         } else {
-//             entry.target.classList.remove('visible'); // para animar también al subir
-//         }
-//     });
-// }, {
-//     threshold: 0.2
-// });
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('visible');
+        } else {
+            entry.target.classList.remove('visible'); // para animar también al subir
+        }
+    });
+}, {
+    threshold: 0.2
+});
 
-// sections.forEach(section => {
-//     observer.observe(section);
-// });
+sections.forEach(section => {
+    observer.observe(section);
+});
