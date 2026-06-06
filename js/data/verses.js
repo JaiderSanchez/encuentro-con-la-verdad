013e10615567a61ec9a017d7c8248fcc0b8724c6
JaiderSanchez <jaiderfabiansd@gmail.com>
2026-06-05 23:10:26 -0500

    Feat: Achievement: Include logo in the 'Testimonies' and 'Verses' views. Create a new verses.js file for an array of verses by category and display them in the 'Home' view.

---
+++ js/data/verses.js
@@ -0,0 +1,39 @@
+const versesHome = {
+    paz: [
+        {
+            referencia: "Mateo 11:28",
+            texto: "Venid a mí todos los que estáis trabajados y cargados, y yo os haré descansar.",
+            imagen: "img/versiculos/paz/mateo-11-28.jpg"
+        },
+
+        {
+            referencia: "Salmo 62:5",
+            texto: "Alma mía, en Dios solamente reposa.",
+            imagen: "img/versiculos/paz/salmo-62-5.jpg"
+        }
+    ],
+
+    ansiedad: [
+        {
+            referencia: "Filipenses 4:6-7",
+            texto: "Por nada estéis afanosos...",
+            imagen: "img/versiculos/ansiedad/filipenses-4-6-7.jpg"
+        }
+    ],
+
+    amor: [
+        {
+            referencia: "Juan 3:16",
+            texto: "Porque de tal manera amó Dios al mundo...",
+            imagen: "img/versiculos/amor/juan-3-16.jpg"
+        }
+    ],
+
+    proposito: [
+        {
+            referencia: "Jeremías 29:11",
+            texto: "Porque yo sé los pensamientos que tengo acerca de vosotros...",
+            imagen: "img/versiculos/proposito/jeremias-29-11.jpg"
+        }
+    ]
+};
